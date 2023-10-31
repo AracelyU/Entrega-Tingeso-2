c398ee41f177bdab487261a54db7b502dd86a22a
AracelyC1 <aracely.castro@usach.cl>
2023-10-31 12:57:15 -0300

    eliminar algunos importaciones que no se usan

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,21 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeComponent from './components/HomeComponent';
 import StudentComponent from './components/StudentComponent';
-import { ReactKeycloakProvider } from '@react-keycloak/web'
-import keycloak from './keycloak'
 function App() {
   return (
       <div>
-        <ReactKeycloakProvider authClient={keycloak}>
           <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<HomeComponent />} />
-              <Route path= "/lista-estudiantes" element={<StudentComponent />} />
-
-
-            </Routes>
+              <Routes>
+                  <Route path="/" element={<HomeComponent />} />
+                  <Route path= "/lista-estudiantes" element={<StudentComponent />} />
+              </Routes>
           </BrowserRouter>
-        </ReactKeycloakProvider>
+
       </div>
   );
 }
--- react-frontend/src/components/HomeComponent.jsx
+++ react-frontend/src/components/HomeComponent.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import { createGlobalStyle } from 'styled-components'
-import { useKeycloak } from '@react-keycloak/web'
 
 
 export default function Home(){
